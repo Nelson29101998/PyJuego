04414982d402de477423132e1ebe1f65f45206b2
TinCode1998 <nelsonmouatvergara@gmail.com>
2025-03-21 11:28:07 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.locals import *
+
+
+WIDTH, HEIGHT = 500, 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+# constants
+
+
+# variables
+cuadrado = pygame.Rect(0, HEIGHT//2, 60, 60)
+velocidad = [3, -14]
+gravedad = 0.4
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            quit()
+
+    # code here
+    cuadrado.x += velocidad[0]
+    cuadrado.y += velocidad[1]
+    velocidad[1] += gravedad
+    pygame.draw.rect(screen, (255, 255, 255), cuadrado)
+    
+    if cuadrado.y > HEIGHT-60:
+        velocidad[1] = 0
+        gravedad = 0
+
+    pygame.display.update()
+    screen.fill((0, 0, 0))
+    clock.tick(30)
