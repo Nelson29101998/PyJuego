fb133f8f617a63440397e53e2b14e8557e20bf74
TinCode1998 <nelsonmouatvergara@gmail.com>
2025-03-26 16:33:33 -0300

    Refactor la inicialización de la clase Pantalla para usar super() en lugar de la llamada directa a la clase base

--- fondos/fondoPantalla.py
+++ fondos/fondoPantalla.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 class Pantalla(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
