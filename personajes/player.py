b80a0d75f944072e268df2005ceaef06bf452091
TinCode1998 <nelsonmouatvergara@gmail.com>
2025-03-27 11:09:39 -0300

    Refactor el manejo de botones del joystick en la clase Jugador para mejorar la legibilidad y la eficiencia del código

--- personajes/player.py
+++ personajes/player.py
@@ -15,31 +15,28 @@ class Jugador(pygame.sprite.Sprite):  # Jugador de la Nave
         #Los botones del teclado
         teclado = pygame.key.get_pressed()
         
-        #Los botones del joystick
-        arriba = pygame.joystick.Joystick(0).get_button(11)
-        abajo = pygame.joystick.Joystick(0).get_button(12)
-        izquierda = pygame.joystick.Joystick(0).get_button(13)
-        derecha = pygame.joystick.Joystick(0).get_button(14)
+        #Los botones del joystick        
+        buttonJoy = pygame.joystick.Joystick(0)
             
-        if teclado[K_LEFT] or izquierda:
+        if teclado[K_LEFT] or buttonJoy.get_button(13): #Si se presiona la tecla izquierda o el boton 13 del joystick
             self.rect.x -= self.velocidad[0]
             self.velocidad[0] += self.aceleracion
 
-        if teclado[K_RIGHT] or derecha:
+        if teclado[K_RIGHT] or buttonJoy.get_button(14): #Si se presiona la tecla derecha o el boton 14 del joystick
             self.rect.x += self.velocidad[0]
             self.velocidad[0] += self.aceleracion
 
-        if teclado[K_UP] or arriba:
+        if teclado[K_UP] or buttonJoy.get_button(11): #Si se presiona la tecla arriba o el boton 11 del joystick
             self.rect.y += self.velocidad[1]
             self.velocidad[1] -= self.aceleracion
 
-        if teclado[K_DOWN] or abajo:
+        if teclado[K_DOWN] or buttonJoy.get_button(12): #Si se presiona la tecla abajo o el boton 12 del joystick
             self.rect.y -= self.velocidad[1]
             self.velocidad[1] -= self.aceleracion
 
-        if not (teclado[K_LEFT] or izquierda) and not (teclado[K_RIGHT] or derecha):
+        if not (teclado[K_LEFT] or buttonJoy.get_button(13)) and not (teclado[K_RIGHT] or buttonJoy.get_button(14)):
             self.velocidad[0] = 3
 
-        if not (teclado[K_UP] or arriba) and not (teclado[K_DOWN] or abajo):
+        if not (teclado[K_UP] or buttonJoy.get_button(11)) and not (teclado[K_DOWN] or buttonJoy.get_button(12)):
             self.velocidad[1] = -3
         
