cd07cef5e30a903c31a6bf489cf1180e1631b313
TinCode1998 <nelsonmouatvergara@gmail.com>
2025-03-27 12:48:09 -0300

    Agregar detección y manejo de ejes del joystick en la clase Jugador para mejorar el control del movimiento

--- personajes/player.py
+++ personajes/player.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 
 class Jugador(pygame.sprite.Sprite):  # Jugador de la Nave
     def __init__(self, image_file,  x, y):
+        self.cambiarXL3, self.cambiarYL3, self.cambiarXR3, self.cambiarYR3 = 0, 0, 0, 0
         super().__init__()
         self.aceleracion = 0.1
         self.velocidad = [3, -3]
@@ -34,6 +35,18 @@ class Jugador(pygame.sprite.Sprite):  # Jugador de la Nave
         #Los botones del joystick
         if pygame.joystick.get_count() > 0:      
             buttonJoy = pygame.joystick.Joystick(0)
+            ejeXL3 = buttonJoy.get_axis(0) #Eje X L3
+            ejeYL3 = buttonJoy.get_axis(1) #Eje Y L3
+            self.cambiarX += ejeXL3 * 2
+            self.cambiarY += ejeYL3 * 2
+            print("Eje X L3: ", self.cambiarXL3 , ", Eje Y L3: ", self.cambiarYL3)
+            
+            ejeXR3 = buttonJoy.get_axis(2) #Eje X R3
+            ejeYR3 = buttonJoy.get_axis(3) #Eje Y R3
+            self.cambiarX += ejeXR3 * 2
+            self.cambiarY += ejeYR3 * 2
+            print("Eje X R3: ", self.cambiarXR3 , ", Eje Y R3: ", self.cambiarYR3)
+            
             if buttonJoy.get_button(13): #Si se presiona la tecla izquierda o el boton 13 del joystick
                 self.rect.x -= self.velocidad[0]
                 self.velocidad[0] += self.aceleracion
