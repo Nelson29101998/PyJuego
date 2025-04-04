4fbfb1aa604f49e53f69d1e59e5ea0afca399583
TinCode1998 <nelsonmouatvergara@gmail.com>
2025-04-04 11:57:19 -0300

    Eliminar configuración de fuerzas de gatillos en la función principal

--- main.py
+++ main.py
@@ -91,13 +91,6 @@ def main():
     fondo = fondoPantalla.Pantalla("assets/img/fondo/galaxia.jpg", [0, 0])
     player_group.add(jugador)
     fondo_group.add(fondo)
-
-    trigger_forces = {}
-
-    # Establecer las fuerzas
-    trigger_forces[1] = 0  # Fuerza en el punto 1
-    trigger_forces[2] = 80  # Fuerza en el punto 2
-    trigger_forces[3] = 200  # Fuerza en el punto 3
     
     numero = 0
 
